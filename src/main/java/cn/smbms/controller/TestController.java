a8b144ca1fa5c78276d21a1122974d33e01a7fb9
SpringW <3002763504@qq.com>
2019-02-28 15:59:26 +0800

    组员B添加测试controller

---
+++ src/main/java/cn/smbms/controller/TestController.java
@@ -0,0 +1,13 @@
+package cn.smbms.controller;
+
+/**
+ * ClassName: TestController <br/>
+ * Description: <br/>
+ * date: 2019/2/28 15:58<br/>
+ *
+ * @author LucyBoy<br />
+ * @since JDK 1.8
+ */
+public class TestController {
+    //添加测试controller
+}
